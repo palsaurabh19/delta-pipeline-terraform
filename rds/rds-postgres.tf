ba52968bda4f02474bdd3f93840e9a48e06902c3
palsaurabh19 <97594685+palsaurabh19@users.noreply.github.com>
2025-05-23 21:09:32 -0400

    Update rds-postgres.tf

--- rds/rds-postgres.tf
+++ rds/rds-postgres.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "hms_db" {
   engine             = "postgres"
   instance_class     = "db.t3.micro"
   allocated_storage  = 20
-  name               = "hmsdb"
+  db_name               = "hmsdb"
   username           = "hmsuser"
   password           = "SecurePassword123"
   skip_final_snapshot = true
