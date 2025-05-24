84b1b6542aa6494205b5b20a4abd8725de3bad32
palsaurabh19 <97594685+palsaurabh19@users.noreply.github.com>
2025-05-23 21:14:29 -0400

    Update eks-cluster.tf

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -1,20 +1,17 @@
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.21.0"
-
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "8.0.0"  # Compatible with Terraform 1.12
   cluster_name    = "delta-cluster"
   cluster_version = "1.27"
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.private_subnets  
-  enable_irsa     = true
 
-  eks_managed_node_groups = {
-    general = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 3
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+  enable_irsa = true
 
-      instance_types = ["t3.large"]
+  worker_groups = [
+    {
+      instance_type = "t3.large"
+      asg_desired_capacity = 2
     }
-  }
+  ]
 }
