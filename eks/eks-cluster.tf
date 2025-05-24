e59da638d8ea1d38ab68fd1d92f6cc4b0637820e
palsaurabh19 <97594685+palsaurabh19@users.noreply.github.com>
2025-05-23 20:59:51 -0400

    Update eks-cluster.tf

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -1,17 +1,23 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.21.0"  
+
   cluster_name    = "delta-cluster"
   cluster_version = "1.27"
-  subnets         = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
-  enable_irsa     = true
+
+  subnets = module.vpc.private_subnets
+  vpc_id  = module.vpc.vpc_id
+
+  enable_irsa = true
 
   node_groups = {
     general = {
       desired_capacity = 3
       max_capacity     = 3
       min_capacity     = 1
-      instance_type    = "t3.large"
+
+      instance_types = ["t3.large"]  
     }
   }
 }
+
